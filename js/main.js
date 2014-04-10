ab07ebf71a33b29677588b2acde9cb167a50f90e
Jason Marmon <jtmarmon@gmail.com>
2014-04-09 23:56:23 -0400

    circles and animations yay

--- js/main.js
+++ js/main.js
@@ -7,6 +7,64 @@ d - #6ed0ff
 e - #06ff81
 
 */
+var ticket;
+function animateMoney()
+{
+	var money = new Array();
+	var rightVals = new Array();
+		rightVals[0] = 17.5;
+		rightVals[1] = 22.5;
+		rightVals[2] = 12.5;
+	var initBottom = 30;
+	for(var i=0; i<3;i++)
+	{
+		money[i] = document.createElement("p");	
+		money[i]= $(money[i]);
+		money[i].append("$");
+		money[i].css({
+			"color":'#0AFF64',
+			'display':'inline-block',
+			'text-shadow':'none',
+			'position':'absolute',
+			'right':rightVals[i] + '%',
+			'bottom':initBottom + '%',
+			'z-index':'60'
+		});
+		initBottom +=10;
+	$("#money-animation-holder").append(money[i]);
+	}
+	for(var i=0; i<3; i++)
+	{
+		money[i].animate({'bottom':'+=100','opacity':'0'},1000,"linear");
+	}
+}
+function checkTicketLoc()
+{
+	var holder = $("#ticket-holder");
+	if(holder.offset().left> $("#circle3").offset().left + 60)
+	{
+		holder.hide();
+		holder.stop();
+		animateMoney();
+	}
+	setTimeout(checkTicketLoc);
+}
+function scrollTicket()
+{
+	var holder = $("#ticket-holder");
+	ticketLoc = holder.offset().left;
+	holder.animate({"left":ticketLoc+=95 + "%"},20000, "linear");
+	checkTicketLoc();
+	holder.queue(function()
+	{
+		$(this).dequeue();
+	});
+}
+function stopTicket()
+{
+	$("#ticket-holder").clearQueue();
+	$("#ticket-holder").stop();
+}
 var colors = new Array();
 /*colors[0] = "#ff7cf4";
 colors[1] = "#ff616f";
@@ -29,9 +87,20 @@ borderColors[0] = "#FFD700";
 borderColors[1] = "#C0C0C0";
 var logoColorIndex = 0;
 var borderColorIndex = 0;
-
+var ticketLoc = "10";
+var timeoutHolder;
 $(document).ready(function() {
 	//animateElements();
+	
+ticket = document.createElement("img");
+ticket.setAttribute("src", "img/ticket.png");
+ticket.setAttribute("height","100%");
+ticket.setAttribute("z-index","20");
+ticket.setAttribute("opacity","1");
+document.getElementById("ticket-holder").appendChild(ticket);
+	
+	$('#info').hover(scrollTicket,stopTicket);
+
 	$('#coin').animate({
         'opacity':'1'
     },2000);
@@ -39,6 +108,7 @@ $(document).ready(function() {
     	'color':'gold'
     })
 });
+
 function spectrum(){
 		borderColorIndex++;
 		borderColorIndex%=borderColors.length;
